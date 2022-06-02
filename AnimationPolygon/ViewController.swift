10da0cf14be497168fd10d9d90776b1fc671c769
Nathan Furman <ncfusn@gmail.com>
2022-06-02 11:28:38 +0300

    Initial Commit

---
+++ AnimationPolygon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationPolygon
+//
+//  Created by Nathan Furman on 02/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
